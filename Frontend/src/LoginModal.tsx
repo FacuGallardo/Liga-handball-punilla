be373b0714c5a675ea61f898742219c40de796af
FacuGallardo <gashardo23@gmail.com>
2026-03-31 21:21:03 -0300

    xd

--- Frontend/src/LoginModal.tsx
+++ Frontend/src/LoginModal.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { API_URL } from "../config/api";
+import { API_URL } from "./config/api";
 
 // --- Interfaz de Props ---
 interface LoginModalProps {
